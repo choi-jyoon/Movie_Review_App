40b7403935a84b7defd71289ac296123ae39ac12
choi-jyoon <72444141+choi-jyoon@users.noreply.github.com>
2024-03-14 14:09:42 +0900

    feat: 메인 화면 구성

--- src/App.js
+++ src/App.js
@@ -1,54 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
-import Movie from './pages/Movie';
+import Home from './pages/Home';
 import './App.css'
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-
-  getMovies = async () => {
-    const { data: {movies} } = await axios.get('/movies');
-  
-    console.log(movies);
-    this.setState({movies, isLoading:false});
-  };
-
-  componentDidMount() {
-    this.getMovies(); // 컴포넌트가 마운트되면 영화 데이터 가져오기
-  }
-
-  render() {
-    const {isLoading, movies} = this.state;
-    return (
-      <section className="container">
-        {isLoading? (
-          <div className="loader">
-            <span className="loader_text">loading...</span>
-          </div>
-        ):
-        (
-          <div className="movies">
-            {
-              movies.map(movie => (
-                <Movie 
-                key = {movie.id}
-                id = {movie.id}
-                title = {movie.title}
-                percent = {movie.percent}
-                release = {movie.release}
-                img = {movie.img}
-                />
-              ))
-            }
-          </div>
-        )} 
-      </section>
-    );
-  }
+function App(){
+  return <Home/>;
 }
 
 export default App;
